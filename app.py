abe0ff3b7ad75dedfcde7bd7f4f1f1a64a23168d
Udomsak Chundang <udomsak.chundang@gmail.com>
2020-11-19 15:16:05 +0700

    Add rq-dashboard for Queues monitoring support

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from flask_rq2 import RQ
+import rq_dashboard
 
+'''
+For production work should filter or implement access control to end-points 
+'''
 app = Flask(__name__)
+
+app.config.from_object(rq_dashboard.default_settings)
+app.register_blueprint(rq_dashboard.blueprint, url_prefix="/monitor-queues")
 
 
 @app.route('/')
@@ -9,4 +17,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    RQ(app)
+    # app.run()
